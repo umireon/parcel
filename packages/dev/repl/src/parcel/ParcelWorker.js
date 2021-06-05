00c5eef39ab2eaed9b7cf0b09f318c08e3f2cbea
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-06 00:52:14 +0200

    IDBCache

--- packages/dev/repl/src/parcel/ParcelWorker.js
+++ packages/dev/repl/src/parcel/ParcelWorker.js
@@ -13,6 +13,7 @@ import {makeDeferredWithPromise} from '@parcel/utils';
 // import {NodePackageManager} from '@parcel/package-manager';
 import configRepl from '@parcel/config-repl';
 import {generatePackageJson, nthIndex} from '../utils/';
+import {IDBCache} from './IDBCache';
 import path from 'path';
 import {yarnInstall} from './yarn.js';
 import {BrowserPackageManager} from './BrowserPackageManager.js';
@@ -186,6 +187,7 @@ async function setup(assets, options) {
     defaultConfig: '/.parcelrc',
     inputFS: fs,
     outputFS: fs,
+    cache: new IDBCache(),
     defaultTargetOptions: {
       distDir: PathUtils.DIST_DIR,
       publicUrl: options.publicUrl || undefined,
